d16e9e6f57c2fcbcd7be59846db8fc4a88773d3d
Wes <wesleySalesberry@hotmail.com>
2022-11-28 18:07:24 -0500

    Added a check incase no projects were displayed

--- frontend/src/component/Projects/ProjectsMain.js
+++ frontend/src/component/Projects/ProjectsMain.js
@@ -28,45 +28,48 @@ export const ProjectMain = () => {
   }, [language])
 
   const title = language === '' ? "All" : capitalizeFirstLetter(language) 
-
-  return (
-   <Container className="border rounded shadow my-5">
-    <h3 className="text-center my-3">{ title } Projects</h3>
-    <section className="projects-list px-3 py-5 p-md-5">
-      <Container>
-        <div className="text-center">
-          <ul id="filters" className="filters mb-5 mx-auto ps-0">
-            <li
-              className={`type ${activeId === 0 ? 'active' : ''} mb-3 mb-lg-0`}
-              onClick={() => getProject('', 0)}
-            >
-              All
-            </li>
-            {
-              languages.map((itm, idx) => (
+  if(project){
+    return <p>Nothing to display</p>
+  }else{
+    return (
+      <Container className="border rounded shadow my-5">
+        <h3 className="text-center my-3">{ title } Projects</h3>
+        <section className="projects-list px-3 py-5 p-md-5">
+          <Container>
+            <div className="text-center">
+              <ul id="filters" className="filters mb-5 mx-auto ps-0">
                 <li
-                key={idx+1}
-                className={`type ${activeId === idx+1 ? 'active' : ''} mb-3 mb-lg-0`}
-                onClick={() => getProject(itm, idx+1)}
-              >
-                { capitalizeFirstLetter(itm) }
-              </li>
+                  className={`type ${activeId === 0 ? 'active' : ''} mb-3 mb-lg-0`}
+                  onClick={() => getProject('', 0)}
+                >
+                  All
+                </li>
+                {
+                  languages.map((itm, idx) => (
+                    <li
+                    key={idx+1}
+                    className={`type ${activeId === idx+1 ? 'active' : ''} mb-3 mb-lg-0`}
+                    onClick={() => getProject(itm, idx+1)}
+                  >
+                    { capitalizeFirstLetter(itm) }
+                  </li>
+                  ))
+                }
+              </ul>
+            </div>
+          </Container>
+          <ProjectCardLayout>
+            {
+              project.map(itm => (
+                <ProjectCard
+                  key={itm._id}
+                  itm={itm} 
+                />
               ))
             }
-          </ul>
-        </div>
+          </ProjectCardLayout>
+        </section>
       </Container>
-      <ProjectCardLayout>
-        {
-          project.map(itm => (
-            <ProjectCard
-              key={itm._id}
-              itm={itm} 
-            />
-          ))
-        }
-      </ProjectCardLayout>
-    </section>
-   </Container>
-  )
+    )
+  }
 }
