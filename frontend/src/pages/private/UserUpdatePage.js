a83db6480fc885b7fd0ce1217a510a6fbdf0dc3d
Wes <wesleySalesberry@hotmail.com>
2022-11-29 10:21:23 -0500

    Created the update user page

---
+++ frontend/src/pages/private/UserUpdatePage.js
@@ -0,0 +1,10 @@
+import { BaseLayout } from "../../component/Layouts/BaseLayout"
+import { UserUpdateForm } from "../../component/Forms/UserUpdateForm"
+
+export const UserUpdatePage = () => {
+  return(
+    <BaseLayout>
+      <UserUpdateForm />
+    </BaseLayout>
+  )
+}
