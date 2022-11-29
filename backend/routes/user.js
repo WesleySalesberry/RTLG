246879f731583422ff187363b58342569d8dd5d1
Wes <wesleySalesberry@hotmail.com>
2022-11-28 19:33:56 -0500

    Updated routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router()
 
-import { allUsers, getUser, login, register, removeUser, students, updateUser, user } from '../controllers/User.js'
+import { allUsers, getUser, login, register, removeUser, students, updateUser, user, userRemove } from '../controllers/User.js'
 
 import { protect } from '../middleware/protect.js'
 import { authorize } from '../middleware/authorize.js'
@@ -10,10 +10,11 @@ router.route('/')
   .get(protect, authorize('admin'), allUsers)
   .get(protect, authorize('user', 'student', 'admin'), user)
   .post(login)
-
+  
 router.route('/profile')
   .get(protect, authorize('user', 'student', 'admin'), user)
   .put(protect, authorize('student', 'admin'), updateUser)
+  .delete(protect, authorize('user', 'student'), userRemove)
 
 router.route('/register')
   .post(register)
