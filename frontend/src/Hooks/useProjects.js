7c0b2608eb5ddf4ed7e1013281d6f66419630a9b
Wes <wesleySalesberry@hotmail.com>
2022-12-01 15:25:15 -0500

    Cleaned up get project

--- frontend/src/Hooks/useProjects.js
+++ frontend/src/Hooks/useProjects.js
@@ -1,22 +1,20 @@
 import { useState } from "react"
 import Project from "../api/Project"
 
-export const useGetProjects = (language="") => {
+export const useGetProjects = () => {
   const [ error, setError ] = useState(null)
   const [ isLoading, setIsLoading ] = useState(null)
   const [ project, setProject ] = useState([])
 
-  const getProjects = async (id) => {
+  const getProjects = async (language="") => {
     setIsLoading(true);
 
     const data = await Project.project.allProjects(language)
 
     if(data.success === true){
       setProject(data.data)
-      
     }else{
       setError(data.message)
-      
     }
     setIsLoading(false)
   }
